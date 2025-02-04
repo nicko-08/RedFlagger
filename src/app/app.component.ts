da381baa4c8508989845c2b1565ca0af2d73b397
nicko0508 <nickorodavia0508@gmai.com>
2025-02-04 15:27:24 +0800

    add files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SignUpComponentComponent } from "./components/components/sign-up-component.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SignUpComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
