a60d85a92124455fc2590c024645db46c1b0973a
nicko0508 <nickorodavia0508@gmai.com>
2025-02-07 01:22:21 +0800

    Homepage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SignUpComponent } from './components/components/sign-up/sign-up.component';
-import { HeaderComponent } from "./components/components/header/header.component";
+import { HeaderComponent } from "./components/components/navbar/header.component";
 import { FooterComponent } from "./components/components/footer/footer.component";
+import { HomeComponent } from "./components/components/home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [SignUpComponent, HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, HomeComponent, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
