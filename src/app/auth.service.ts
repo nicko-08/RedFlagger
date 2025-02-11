150de1e6241f55c8c852ca61ace6c4c415d12438
Ghaztlyy <ghaztlyy@gmail.com>
2025-02-11 16:59:24 +0800

    AUth database to user database

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,6 +2,8 @@ import { Injectable, signal } from '@angular/core';
 import { environment } from '../environments/environment.development';
 import { AuthResponse, createClient } from '@supabase/supabase-js';
 import { BehaviorSubject, from, Observable } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,13 @@ export class AuthService {
   supabase = createClient(environment.supabaseURL, environment.supabaseKey);
   private isLoggedInSubject = new BehaviorSubject<boolean>(false);
   userStatus = signal<any>(null);
-
   isLoggedIn$ = this.isLoggedInSubject.asObservable();
-
   currentUser = signal<{email: string, username: string} | null > (null) ;
+
+  constructor(private http: HttpClient){
+
+  }
+  
   register(email: string, username: string, password: string): Observable<AuthResponse> {
     const promise = this.supabase.auth.signUp({
       email,
@@ -45,5 +50,26 @@ export class AuthService {
     return data.session;
   }
 
+listenForAuthChanges(): void{
+  this.supabase.auth.onAuthStateChange(async(event, session)=>{
+    if(event === 'SIGNED_IN' && session?.user?.email_confirmed_at){
+      const accessToken = session.access_token;
+      let stringe = "Bearer " + accessToken;
+      const baseHeaders = new HttpHeaders().set('Authorization', stringe);
 
+      console.log(baseHeaders.get('Authorization'))
+      
+      this.http.post<{message : string}>('https://redflagger-api.et.r.appspot.com/user/new',{},{headers: baseHeaders}).subscribe({
+        next: (response: {message: string})=>{
+          console.log('User inserted Successfully:', response);
+        },
+        error: (error: any) =>{
+          console.error('Error inserting user:', error);
+        },
+      });
+    }
+  });
 }
+}
+
+
