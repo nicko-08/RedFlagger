cb9c2761eb8bc5659397a1187f772c0636b454ee
Raindeeru <derv.gonzales17@gmail.com>
2025-06-21 15:17:04 +0800

    changing profile pic

--- src/app/components/components/user-profile/user-profile.component.ts
+++ src/app/components/components/user-profile/user-profile.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../../../auth.service';
 import { CommonModule } from '@angular/common';
 import { ReportUserProfileComponent } from '../report-user-profile/report-user-profile.component';
 import { ReviewUserProfileComponent } from '../review-user-profile/review-user-profile.component';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-profile',
@@ -22,14 +23,19 @@ export class UserProfileComponent implements OnInit, OnDestroy {
   private authSub?: Subscription;
 
   iconFileName = 'default.svg';
-  iconColor = 'bg-red-500';
-  bgColor = 'bg-blue-500';
+
+  userProfileImage = "default";
+  userProfileForeground = "white";
+  userProfileBackgroud = "red";
+
+  http = inject(HttpClient);
 
   ngOnInit() {
     this.authService.supabase.auth.getUser().then(({ data }) => {
       if (data?.user) {
         this.username = data.user.user_metadata['username'] || null;
         this.userId = data.user.id || '';
+        this.getUserInfo();
       } else {
         this.router.navigate(['/home']);
       }
@@ -50,5 +56,20 @@ export class UserProfileComponent implements OnInit, OnDestroy {
 
   goToSettings() {
     this.router.navigate(['/profile-settings']);
+  }
+
+  getUserInfo(){
+    this.http
+      .get<any>(`https://redflagger-api-10796636392.asia-southeast1.run.app/userProfile?user_id=${this.userId}`)
+      .subscribe({
+        next: (data) => {
+          console.log(data)
+          this.userProfileImage = data.user.PROFILE_IMAGE;
+          console.log(this.userProfileImage);
+        },
+        error: (err) => {
+          console.error('Failed to fetch reviews:', err);
+        }
+      });
   }
 }
