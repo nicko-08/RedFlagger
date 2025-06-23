16803b14070d50317142b6181ab65637372402d5
Ghastly <Ghaztlyy@gmail.com>
2025-06-23 22:14:52 +0800

    OMG OK NA QUERY PARAMS

--- src/app/components/components/report-user-profile/report-user-profile.component.ts
+++ src/app/components/components/report-user-profile/report-user-profile.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule, DatePipe } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, Input, SimpleChanges } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-report-user-profile',
@@ -12,6 +13,7 @@ export class ReportUserProfileComponent {
 @Input() userId!: string;
   reports: any[] = [];
   http = inject(HttpClient);
+  router = inject(Router);
   loading = true;
 
   ngOnChanges(changes: SimpleChanges) {
@@ -32,4 +34,21 @@ export class ReportUserProfileComponent {
         });
     }
   }
+
+  goToPostReports(postId: number): void {
+  const api = `https://redflagger-api-10796636392.asia-southeast1.run.app/post?post_id=${postId}`;
+  this.http.get<any>(api).subscribe({
+    next: (data) => {
+      if (data.POST_URL) {
+        // Navigate using query param
+        this.router.navigate(['/post-reports'], {
+          queryParams: { input: data.POST_URL }
+        });
+      }
+    },
+    error: (err) => {
+      console.error('Error fetching post_url:', err);
+    }
+  });
+}
 }
